385df3fa9112a01e3d84a10885f419f1f1e99b42
nightskyshop1023 <nightskyshop1023@naver.com>
2023-09-20 16:06:11 +0900

    Depoly Website 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import Header from '@/components/Header';
+import Container from '@/components/Container';
+import { ThemeProvider } from '@/lib/ThemeContext';
+import '@/styles/global.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Codeitmall</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider>
+        <Header />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  )
 }
